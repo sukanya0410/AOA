a020059c6db19690f1b50fd11446835dd21b039a
sukanya jadhav <sukanya.s2409387209@vcet.edu.in>
2025-03-11 15:03:03 +0530

    Add files via upload

---
+++ BellmanFord.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+public class BellmanFord {
+    static class Edge {
+        int src, dest, weight;
+        Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+
+    public static void bellmanFord(Edge[] edges, int V, int E, int src) {
+        int[] dist = new int[V];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        for (int i = 1; i < V - 1; i++) {
+            for (int j = 0; j < E; j++) {
+                int u = edges[j].src;
+                int v = edges[j].dest;
+                int weight = edges[j].weight;
+                if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v]) {
+                    dist[v] = dist[u] + weight;
+                }
+            }
+        }
+
+        for (int i = 0; i < E; i++) {
+            int u = edges[i].src;
+            int v = edges[i].dest;
+            int weight = edges[i].weight;
+            if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v]) {
+                System.out.println("Graph contains negative weight cycle");
+                return;
+            }
+        }
+
+        for (int i = 0; i < V; i++) {
+            System.out.println("Distance from source to vertex " + i + " is " + dist[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 5;
+        int E = 8;
+        Edge[] edges = new Edge[E];
+        edges[0] = new Edge(0, 1, -1);
+        edges[1] = new Edge(0, 2, 4);
+        edges[2] = new Edge(1, 2, 3);
+        edges[3] = new Edge(1, 3, 2);
+        edges[4] = new Edge(1, 4, 2);
+        edges[5] = new Edge(3, 2, 5);
+        edges[6] = new Edge(3, 1, 1);
+        edges[7] = new Edge(4, 3, -3);
+
+        bellmanFord(edges, V, E, 0);
+    }
+}
